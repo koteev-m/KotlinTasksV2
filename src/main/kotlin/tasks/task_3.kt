fa6f4f1ee721def2d9417afd3d5786255b691dfd
maksimmartynov <1812.mms@gmail.com>
2024-01-22 16:17:31 +0300

    KT-1-3 Gagarin day

---
+++ src/main/kotlin/tasks/task_3.kt
@@ -0,0 +1,14 @@
+package tasks
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
