d293ac3cc9319c8fa791802f8048b73977c39291
maksimmartynov <1812.mms@gmail.com>
2024-01-22 16:32:49 +0300

    KT-1-4 types

---
+++ src/main/kotlin/tasks/task_4.kt
@@ -0,0 +1,10 @@
+package tasks
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Int = 27
+    val partOfDay: Double = 0.075
+    val seconds: Int = 6480
+    val typeOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+}
