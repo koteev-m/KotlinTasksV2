5743bed7d295b45d562edd808f7e8ce5b84dda46
maksimmartynov <1812.mms@gmail.com>
2024-01-23 16:32:08 +0300

    KT-2-1 average points

---
+++ src/main/kotlin/tasks/arithmetic/task_2_1.kt
@@ -0,0 +1,12 @@
+package tasks.arithmetic
+
+fun main() {
+    val totalStudents = 4f
+    val firstStudentPoints = 3f
+    val secondStudentPoints = 4f
+    val thirdStudentPoints = 3f
+    val fourthStudentPoints = 5f
+
+    val averagePoint: Float = (firstStudentPoints + secondStudentPoints + thirdStudentPoints + fourthStudentPoints) / totalStudents
+    println(averagePoint)
+}
