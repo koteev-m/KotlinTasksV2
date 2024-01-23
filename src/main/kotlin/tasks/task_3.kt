a0e3f105414fd402e34afdf0c95a8f0b0844956f
maksimmartynov <1812.mms@gmail.com>
2024-01-23 14:51:47 +0300

    KT-1-3 Gagarin time string type

--- src/main/kotlin/tasks/task_3.kt
+++ src/main/kotlin/tasks/task_3.kt
@@ -2,13 +2,13 @@ package tasks
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
