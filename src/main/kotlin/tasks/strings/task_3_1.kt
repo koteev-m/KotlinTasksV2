46b71e452ffee5c1e3c6db12193d7132c451000a
maksimmartynov <1812.mms@gmail.com>
2024-02-05 13:15:48 +0300

    KT-3-1 greetings

---
+++ src/main/kotlin/tasks/strings/task_3_1.kt
@@ -0,0 +1,13 @@
+package tasks.strings
+
+fun main() {
+    val name = "Иван!"
+    var greeting = "Добрый день, "
+
+    val dayGreeting = "$greeting$name"
+    println(dayGreeting)
+
+    greeting = "Добрый вечер, "
+    val eveningGreeting = "$greeting$name"
+    println(eveningGreeting)
+}
