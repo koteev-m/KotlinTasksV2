303d214d99809889feec32feaa07aa853fe90463
maksimmartynov <1812.mms@gmail.com>
2024-02-05 12:43:23 +0300

    KT-2-5 interest rate in one number

--- src/main/kotlin/tasks/arithmetic/task_2_5.kt
+++ src/main/kotlin/tasks/arithmetic/task_2_5.kt
@@ -3,8 +3,9 @@ package tasks.arithmetic
 import kotlin.math.pow
 
 fun main() {
+
     val initialDeposit = 70000
-    val annualInterestRate = 16.7 / 100
+    val annualInterestRate = 0.167
     val years = 20
 
     val futureValue = initialDeposit * (1 + annualInterestRate).pow(years)
