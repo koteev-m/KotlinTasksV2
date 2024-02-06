43709090008eda08df1f6598591cf97b98f14973
maksimmartynov <1812.mms@gmail.com>
2024-02-06 16:42:24 +0300

    KT-3-2 change name and age

---
+++ src/main/kotlin/tasks/strings/task_3_2.kt
@@ -0,0 +1,11 @@
+package tasks.strings
+
+fun main() {
+    var fullName = "Андреева Татьяна Сергеевна"
+    var age = 20
+    println("$fullName, $age")
+
+    fullName = "Сидорова Татьяна Сергеевна"
+    age = 22
+    println("$fullName, $age")
+}
