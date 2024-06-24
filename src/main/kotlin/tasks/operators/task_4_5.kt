cac9257e1b80949cec80d3d9d7997c80f49b6838
koteev-m <1812.mms@gmail.com>
2024-06-24 08:53:03 +0300

    KT-4-5 added const val

--- src/main/kotlin/tasks/operators/task_4_5.kt
+++ src/main/kotlin/tasks/operators/task_4_5.kt
@@ -2,7 +2,7 @@ package tasks.operators
 
 fun main() {
     println("наличие повреждений корпуса(true/false): ")
-    val damage = readln().toBoolean()
+    var damage = readln().toBoolean()
 
     println("текущий состав экипажа: ")
     val numberOfCrew = readln().toInt()
@@ -13,13 +13,22 @@ fun main() {
     println("благоприятность метеоусловий(true/false): ")
     val goodWeather = readln().toBoolean()
 
-    val departure = (!damage && numberOfCrew in 55..70 && boxesOfProvision > 50 && goodWeather) ||
-            (damage && numberOfCrew == 70 && goodWeather && boxesOfProvision > 50)
-
-    if (departure){
+    if (((SHIP_WITHOUT_DAMAGE.also { damage = it }) && (numberOfCrew in MIN_CREW..MAX_CREW) &&
+        (boxesOfProvision >= MIN_PROVISION_BOXES) && (goodWeather == SUNNY_WTHR)) ||
+        ((damage == MINOR_DAMAGE) && (numberOfCrew == OPTIMAL_CREW)) && (goodWeather == SUNNY_WTHR) &&
+        (boxesOfProvision >= MIN_PROVISION_BOXES)){
         println("Корабль может отправиться в плавание.")
-    }else println("Корабль не может отправиться в плавание.")
+    } else println("Корабль не может отправиться в плавание.")
+
 }
+
+const val SUNNY_WTHR = true
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val OPTIMAL_CREW = 70
+const val MIN_PROVISION_BOXES = 50
+const val SHIP_WITHOUT_DAMAGE = true
+const val MINOR_DAMAGE = true
 
 
 
