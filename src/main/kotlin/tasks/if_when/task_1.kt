9f10cd020b64ddb294b56b87288b1eaea784265a
koteev-m <1812.mms@gmail.com>
2024-07-01 12:03:29 +0300

    KT-5-1 solving task

---
+++ src/main/kotlin/tasks/if_when/task_1.kt
@@ -0,0 +1,12 @@
+package tasks.if_when
+
+fun main() {
+
+    println("Сложите 2 и 3, и напишите результат")
+    val result = readln().toInt()
+
+    val textResult = if (result == 5) "Добро пожаловать!"
+    else "Доступ запрещен."
+
+    println(textResult)
+}
