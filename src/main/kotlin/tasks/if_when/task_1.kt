43c57792b1e33c63c43ac5aedc25e2af14f42aac
koteev-m <1812.mms@gmail.com>
2024-07-01 12:16:41 +0300

    KT-5-1 solving task

--- src/main/kotlin/tasks/if_when/task_1.kt
+++ src/main/kotlin/tasks/if_when/task_1.kt
@@ -2,7 +2,7 @@ package tasks.if_when
 
 fun main() {
 
-    println("Сложите 2 и 3, и напишите результат:")
+    println("Сложите 2 и 3, и напишите результат")
     val result = readln().toInt()
 
     val textResult = if (result == 5) "Добро пожаловать!"
